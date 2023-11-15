12f5a9d6279c1c69b9b3f800bf8e7d81a98910d0
Jimmy MARTEL <jimmy.martel@epitech.eu>
2023-11-15 17:11:43 +0100

    Add base

---
+++ src/test/java/com/test/shedlocktest/ShedlockTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.shedlocktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShedlockTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
